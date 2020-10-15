886cd612bfa6212cc890f770ef88358444f1e041
p00gz <bloopsocket@gmail.com>
2020-10-14 22:52:55 -0500

    added aws dockerrun

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 test('renders learn react link', () => {});
 =======
 test('renders learn react link', () => {
@@ -11,3 +12,6 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 >>>>>>> 2e26135b46affb4aa1acfb2d67fecd4cd9035ff3
+=======
+test('renders learn react link', () => {});
+>>>>>>> aa96c6e (first commit)
